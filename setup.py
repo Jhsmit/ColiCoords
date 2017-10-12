627ca4043711f7a7f7e163ebeafda5ae070e1699
Jochem Smit <j.h.smit@rug.nl>
2017-10-12 12:46:00 +0200

    added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='CellCoordinatesMK2',
+    version='0.0.1',
+    description='Create a coordinate system in banana-shaped cells.',
+    author='Jochem Smit',
+    author_email='j.h.smit@rug.nl',
+    packages=['cellcoordinates'],
+    keywords='cell fitting ecoli coordinates',
+    #install requires = [ lots of stuff ]
+)
