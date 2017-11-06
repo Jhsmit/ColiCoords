6880a440d41a5764df7a6ff65872876df6ab153b
Jochem Smit <j.h.smit@rug.nl>
2017-11-06 22:26:23 +0100

    added save/load for cell lists

--- colicoords/fileIO.py
+++ colicoords/fileIO.py
@@ -4,68 +4,119 @@ import tifffile
 import os
 from xml.etree import cElementTree as etree
 import warnings
-from colicoords.cell import Cell
+from colicoords.cell import Cell, CellList
 from colicoords.data_models import Data
 
 
 def save(file_path, cell_obj, imagej=False):
     ext = os.path.splitext(file_path)[1]
-    if ext == '.cc' or '':
-        if ext == '':
-            file_path += '.cc'
+
+    if isinstance(cell_obj, Cell):
+        if ext == '.cc' or '':
+            if ext == '':
+                file_path += '.cc'
+
+            with h5py.File(file_path, 'w') as f:
+                name = '_cell' if cell_obj.name is None else cell_obj.name
+                cell_grp = f.create_group(name)
+                _write_cell(cell_grp, cell_obj)
+
+        elif ext == '.tif' or '.tiff':
+            raise NotImplementedError()
+            # with tifffile.TiffWriter(file_path, imagej=imagej) as tif:
+            #     for k, v in cell_obj.data.data_dict.items():
+            #         if v is not None:
+            #             if imagej:
+            #                 if v.dtype == 'int32':
+            #                     print(k)
+            #                     print('something is int32')
+            #                     v = v.astype('int16')
+            #
+            #             tif.save(v.astype('int32'), description=k)
+
+    elif isinstance(cell_obj, CellList):
+        if ext == 'cc' or '':
+            if ext == '':
+                file_path += '.cc'
 
         with h5py.File(file_path, 'w') as f:
-            attr_grp = f.create_group('attributes')
+            for c in cell_obj:
+                name = 'None' if c.name is None else c.name
+                cell_grp = f.create_group(name)
+                _write_cell(cell_grp, c)
 
-            attr_grp.attrs.create('r', cell_obj.coords.r)
-            attr_grp.attrs.create('xl', cell_obj.coords.xl)
-            attr_grp.attrs.create('xr', cell_obj.coords.xr)
-            attr_grp.attrs.create('coeff', cell_obj.coords.coeff)
 
-            #todo python 3 compatiblity: https://github.com/h5py/h5py/issues/441
-            if cell_obj.label:
-                attr_grp.attrs.create('label', cell_obj.label.encode())
+def _write_cell(cell_grp, cell_obj):
+    attr_grp = cell_grp.create_group('attributes')
 
-            data_grp = f.create_group('data')
-            for k, v in cell_obj.data.data_dict.items():
-                grp = data_grp.create_group(k)
-                grp.create_dataset(k, data=v)
-                grp.attrs.create('dclass', np.string_(v.dclass))
-                #todo metatdata
+    attr_grp.attrs.create('r', cell_obj.coords.r)
+    attr_grp.attrs.create('xl', cell_obj.coords.xl)
+    attr_grp.attrs.create('xr', cell_obj.coords.xr)
+    attr_grp.attrs.create('coeff', cell_obj.coords.coeff)
 
-    elif ext == '.tif' or '.tiff':
-        with tifffile.TiffWriter(file_path, imagej=imagej) as tif:
-            for k, v in cell_obj.data.data_dict.items():
-                if v is not None:
-                    if imagej:
-                        if v.dtype == 'int32':
-                            print(k)
-                            print('something is int32')
-                            v = v.astype('int16')
+    attr_grp.attrs.create('name', np.string_(cell_obj.name))
 
-                    tif.save(v.astype('int32'), description=k)
+    data_grp = cell_grp.create_group('data')
+    for k, v in cell_obj.data.data_dict.items():
+        grp = data_grp.create_group(k)
+        grp.create_dataset(k, data=v)
+        grp.attrs.create('dclass', np.string_(v.dclass))
+
+
+def _load_cell(cell_grp):
+    data_obj = Data()
+    data_grp = cell_grp['data']
+
+    for key in list(data_grp.keys()):
+        grp = data_grp[key]
+        data_arr = grp[key]
+        dclass = grp.attrs.get('dclass').decode('UTF-8')
+        data_obj.add_data(data_arr, dclass=dclass, name=key)
+
+    c = Cell(data_obj)
+
+    attr_grp = cell_grp['attributes']
+    attr_dict = dict(attr_grp.attrs.items())
+
+    for a in ['r', 'xl', 'xr', 'coeff']:
+        setattr(c.coords, a, attr_dict.get(a))
+
+    name = attr_dict.get('name').decode('UTF-8')
+    c.name = name if name is not 'None' else None
+
+    return c
 
 
 def load(file_path):
     ext = os.path.splitext(file_path)[1]
+
+
+
     if ext == '.cc':
         with h5py.File(file_path, 'r') as f:
+            cell_list = [_load_cell(f[key]) for key in f.keys()]
 
-            data_obj = Data()
-            data_grp = f['data']
-            for key in list(data_grp.keys()):
-                grp = data_grp[key]
-                data_arr = grp[key]
-                dclass = grp.attrs.get('dclass').decode('UTF-8')
-                data_obj.add_data(data_arr, dclass=dclass, name=key)
+            if len(cell_list) == 1:
+                return cell_list[0]
+            else:
+                return CellList(cell_list)
 
-            c = Cell(data_obj)
 
-            attr_grp = f['attributes']
-            attr_dict = dict(attr_grp.attrs.items())
-            for a in ['r', 'xl', 'xr', 'coeff']:
-                setattr(c.coords, a, attr_dict.get(a))
-            c.name = attr_dict.get('label')
+            # data_obj = Data()
+            # data_grp = f['data']
+            # for key in list(data_grp.keys()):
+            #     grp = data_grp[key]
+            #     data_arr = grp[key]
+            #     dclass = grp.attrs.get('dclass').decode('UTF-8')
+            #     data_obj.add_data(data_arr, dclass=dclass, name=key)
+            #
+            # c = Cell(data_obj)
+            #
+            # attr_grp = f['attributes']
+            # attr_dict = dict(attr_grp.attrs.items())
+            # for a in ['r', 'xl', 'xr', 'coeff']:
+            #     setattr(c.coords, a, attr_dict.get(a))
+            # c.name = attr_dict.get('label')
 
         return c
 
--- test/test_fileio.py
+++ test/test_fileio.py
@@ -11,25 +11,43 @@ import numpy as np
 class FileIOTest(ArrayTestCase):
     def setUp(self):
         self.data = generate_testdata('ds3')
-        cell_list = data_to_cells(self.data, pad_width=2, cell_frac=0.5, rotate='Binary')
-        self.cell_obj = cell_list[0]
+        self.cell_list = data_to_cells(self.data, pad_width=2, cell_frac=0.5, rotate='binary')
+        self.cell_obj = self.cell_list[0]
         self.cell_obj.optimize()
 
-    def test_save_load(self):
+    def test_save_load_cells(self):
         save('temp_save.cc', self.cell_obj)
         cell_obj_load = load('temp_save.cc')
 
         for item in ['r', 'xl', 'xr']:
             self.assertEqual(getattr(self.cell_obj.coords, item), getattr(cell_obj_load.coords, item))
 
-        self.assertEqual(self.cell_obj.label, cell_obj_load.label)
+        self.assertEqual(self.cell_obj.name, cell_obj_load.name)
 
         for p1, p2 in zip(self.cell_obj.coords.coeff, cell_obj_load.coords.coeff):
             self.assertEqual(p1, p2)
 
-        #todo these tests alwasys pass!!! (or dont they?) --> they do
-        self.assertTrue(np.all(self.cell_obj.data.binary_img == cell_obj_load.data.binary_img))
-        self.assertTrue(np.all(self.cell_obj.data.flu_Fluorescence == cell_obj_load.data.flu_Fluorescence))
+        self.assertArrayEqual(self.cell_obj.data.binary_img, cell_obj_load.data.binary_img)
+        self.assertArrayEqual(self.cell_obj.data.flu_fluorescence, cell_obj_load.data.flu_fluorescence)
+
+    def test_save_load_cell_list(self):
+        save('temp_save_celllist.cc', self.cell_list)
+        cell_list_load = load('temp_save_celllist.cc')
+
+        self.assertEqual(len(self.cell_list), len(cell_list_load))
+
+        for ci, co in zip(self.cell_list, cell_list_load):
+            for item in ['r', 'xl', 'xr']:
+                self.assertEqual(getattr(ci.coords, item), getattr(co.coords, item))
+
+            self.assertEqual(ci.name, co.name)
+
+            for p1, p2 in zip(ci.coords.coeff, co.coords.coeff):
+                self.assertEqual(p1, p2)
+
+            self.assertArrayEqual(ci.data.binary_img, co.data.binary_img)
+            self.assertArrayEqual(ci.data.flu_fluorescence, co.data.flu_fluorescence)
+
 
 if __name__ == '__main__':
     unittest.main()
