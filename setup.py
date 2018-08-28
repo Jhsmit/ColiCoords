6827cacd08bd7db1da54f6893039000349909536
Jochem Smit <j.h.smit@rug.nl>
2018-08-28 20:51:36 +0200

    changed config setup

--- setup.py
+++ setup.py
@@ -1,12 +1,29 @@
-from distutils.core import setup
+from setuptools import setup
 
 setup(
-    name='colicoords',
-    version='0.0.1',
-    description='Create a coordinate system in curved cells.',
-    author='Jochem Smit',
-    author_email='j.h.smit@rug.nl',
-    packages=['colicoords'],
-    keywords='cell fitting ecoli coordinates',
-    #install requires = [ lots of stuff ]
+    setup_requires=['pbr>=1.9', 'setuptools>=17.1'],
+    pbr=False,
 )
+
+
+
+
+
+
+
+# from distutils.core import setup
+#
+#
+# setup(
+#     name='colicoords',
+#     version='0.0.1',
+#     description='Create a coordinate system in rod-shaped cells.',
+#     author='Jochem Smit',
+#     author_email='j.h.smit@rug.nl',
+#     packages=['colicoords', 'colicoords.gui'],
+#     keywords='cell microscopy ecoli coordinates',
+#     requires=['numpy', 'matplotlib', 'seaborn', 'mahotas', 'scipy', 'h5py'],
+#     # extras_require={'GUI': ['pyqt>=5'],
+#     #                 'CNN': ['tensorflow', 'keras']}
+# )
+
