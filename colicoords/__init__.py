4ca7d207aecc6f4755a70b9d89c982572d9dd1e5
Jochem Smit <j.h.smit@rug.nl>
2018-10-04 16:42:21 +0200

    updated init

--- colicoords/__init__.py
+++ colicoords/__init__.py
@@ -4,6 +4,7 @@ print('You are using ColiCoords version {}'.format(__version__))
 from colicoords.data_models import Data
 from colicoords.cell import Cell, CellList
 from colicoords.preprocess import data_to_cells
+from colicoords.postprocess import align_cells, align_images, align_storm
 from colicoords.plot import CellPlot, CellListPlot
 from colicoords.fileIO import load, save, load_thunderstorm
 from colicoords.models import RDistModel, PSF
