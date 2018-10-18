2ce4a9c624f59848289c298c67d7958165d1d5b1
Jochem Smit <j.h.smit@rug.nl>
2018-10-18 10:30:41 +0200

    removed multiprocessing from testing and made DE fitting deterministic

--- test/test_fitting.py
+++ test/test_fitting.py
@@ -10,6 +10,7 @@ class TestCellFitting(ArrayTestCase):
     def setUp(self):
         f_path = os.path.dirname(os.path.realpath(__file__))
         self.cells = load(os.path.join(f_path, 'test_data', 'test_cells.hdf5'))
+        self.de_kwargs = {'popsize': 10, 'recombination': 0.9, 'seed': 42}
 
     def test_fitting_binary(self):
         resdict = {'a0': 2.004259e+01, 'a1': -2.571423e-01, 'a2': 4.944874e-03,
@@ -28,9 +29,9 @@ class TestCellFitting(ArrayTestCase):
             self.assertAlmostEqual(res.params[key], val, 5)
 
         cell_0 = self.cells[0].copy()
-        res = cell_0.optimize(minimizer=DifferentialEvolution)
+        res = cell_0.optimize(minimizer=DifferentialEvolution, **self.de_kwargs)
 
-        self.assertLessEqual(res.objective_value, 25)
+        self.assertEqual(res.objective_value, 24)
 
     def test_fitting_brightfield(self):
         res_dict = {'a0': 2.059920e+01, 'a1': -2.712269e-01, 'a2': 5.158162e-03,
@@ -48,15 +49,15 @@ class TestCellFitting(ArrayTestCase):
         for key, val in res_dict.items():
             self.assertAlmostEqual(res.params[key], val, 5)
 
+    def test_fitting_brightfield_DE(self):
         cell_0 = self.cells[0].copy()
-        res_dict = {'a0': 1.780838e+01, 'a1': -3.784501e-02, 'a2':7.301865e-04,
-                    'r': 9.244447e+00,'xl': 1.516948e+01, 'xr': 3.650385e+01}
-        deltas = {'a0': 0.5, 'a1': 1e-2, 'a2':1e-3,
-            'r': 0.05,'xl': 1e-2, 'xr': 1e-2}
-        res = cell_0.optimize('brightfield', minimizer=DifferentialEvolution)
-        self.assertLessEqual(res.objective_value, 8185000)
+        res_dict = {'a0': 1.762122e+01, 'a1': -2.272049e-02, 'a2': 4.457729e-04,
+                    'r': 9.243795e+00, 'xl': 1.517070e+01, 'xr': 3.638401e+01}
+
+        res = cell_0.optimize('brightfield', minimizer=DifferentialEvolution, **self.de_kwargs)
+        self.assertAlmostEqual(res.objective_value, 8206601.073967202, 6)
         for k, v in res_dict.items():
-            self.assertAlmostEqual(v, res.params[k], delta=deltas[k])
+            self.assertAlmostEqual(v, res.params[k], 5)
 
     def test_cell_list_optimize(self):
         cells = self.cells[:8].copy()
@@ -102,79 +103,79 @@ class TestCellFitting(ArrayTestCase):
         for r, val in zip(res_list, obj_values):
             self.assertEqual(r.objective_value, val)
 
-    def test_multiprocessing(self):
-        cells = self.cells[:8].copy()
-        res_list = cells.optimize_mp()
+    # def test_multiprocessing(self):
+    #     cells = self.cells[:8].copy()
+    #     res_list = cells.optimize_mp()
+    #
+    #     # Check if the result has been properly substituted in all cell objects
+    #     for r, cell in zip(res_list, cells):
+    #         for k, v in r.params.items():
+    #             self.assertEqual(v, getattr(cell.coords, k))
+    #
+    #     obj_values = [30, 25, 26, 25, 26, 22, 37, 17]
+    #     for r, val in zip(res_list, obj_values):
+    #         self.assertEqual(r.objective_value, val)
+    #
+    #     cells = self.cells[:8].copy()
+    #     res_list = cells.optimize_mp(minimizer=Powell)
+    #
+    #     # Check if the result has been properly substituted in all cell objects
+    #     for r, cell in zip(res_list, cells):
+    #         for k, v in r.params.items():
+    #             self.assertEqual(v, getattr(cell.coords, k))
+    #
+    #     obj_values = [30, 25, 26, 25, 26, 22, 37, 17]
+    #     for r, val in zip(res_list, obj_values):
+    #         self.assertEqual(r.objective_value, val)
 
-        # Check if the result has been properly substituted in all cell objects
-        for r, cell in zip(res_list, cells):
-            for k, v in r.params.items():
-                self.assertEqual(v, getattr(cell.coords, k))
+    # def test_multiprocessing_brightfield(self):
+    #     cells = self.cells[:8].copy()
+    #     res_list = cells.optimize_mp('brightfield')
+    #     print([r.objective_value for r in res_list])
+    #
+    #     # Check if the result has been properly substituted in all cell objects
+    #     for r, cell in zip(res_list, cells):
+    #         for k, v in r.params.items():
+    #             self.assertEqual(v, getattr(cell.coords, k))
+    #
+    #     obj_values = [10016887.123816863, 23617786.697511297, 8999333.084250152, 29339637.49970112,
+    #                   62892422.65259473, 20011819.287397716, 33025293.172053672, 112600585.34296474]
+    #
+    #     for r, val in zip(res_list, obj_values):
+    #         self.assertAlmostEqual(r.objective_value, val, 5)
+    #
+    #     cells = self.cells[:4].copy()
+    #     res_list = cells.optimize_mp(minimizer=DifferentialEvolution)
+    #     print([r.objective_value for r in res_list]) #[23.0, 17.0, 18.0, 22.0, 16.0, 17.0, 25.0, 10.0]
+    #     #
+    #     # Check if the result has been properly substituted in all cell objects
+    #     for r, cell in zip(res_list, cells):
+    #         for k, v in r.params.items():
+    #             self.assertEqual(v, getattr(cell.coords, k))
+    #
+    #     obj_values = [23.0, 17.0, 18.0, 22.0]
+    #     for r, val in zip(res_list, obj_values):
+    #         self.assertLessEqual(r.objective_value, val + 3)
 
-        obj_values = [30, 25, 26, 25, 26, 22, 37, 17]
-        for r, val in zip(res_list, obj_values):
-            self.assertEqual(r.objective_value, val)
-
-        cells = self.cells[:8].copy()
-        res_list = cells.optimize_mp(minimizer=Powell)
-
-        # Check if the result has been properly substituted in all cell objects
-        for r, cell in zip(res_list, cells):
-            for k, v in r.params.items():
-                self.assertEqual(v, getattr(cell.coords, k))
-
-        obj_values = [30, 25, 26, 25, 26, 22, 37, 17]
-        for r, val in zip(res_list, obj_values):
-            self.assertEqual(r.objective_value, val)
-
-    def test_multiprocessing_brightfield(self):
-        cells = self.cells[:8].copy()
-        res_list = cells.optimize_mp('brightfield')
-        print([r.objective_value for r in res_list])
-
-        # Check if the result has been properly substituted in all cell objects
-        for r, cell in zip(res_list, cells):
-            for k, v in r.params.items():
-                self.assertEqual(v, getattr(cell.coords, k))
-
-        obj_values = [10016887.123816863, 23617786.697511297, 8999333.084250152, 29339637.49970112,
-                      62892422.65259473, 20011819.287397716, 33025293.172053672, 112600585.34296474]
-
-        for r, val in zip(res_list, obj_values):
-            self.assertAlmostEqual(r.objective_value, val, 5)
-
-        cells = self.cells[:4].copy()
-        res_list = cells.optimize_mp(minimizer=DifferentialEvolution)
-        print([r.objective_value for r in res_list]) #[23.0, 17.0, 18.0, 22.0, 16.0, 17.0, 25.0, 10.0]
-        #
-        # Check if the result has been properly substituted in all cell objects
-        for r, cell in zip(res_list, cells):
-            for k, v in r.params.items():
-                self.assertEqual(v, getattr(cell.coords, k))
-
-        obj_values = [23.0, 17.0, 18.0, 22.0]
-        for r, val in zip(res_list, obj_values):
-            self.assertLessEqual(r.objective_value, val + 3)
-
-    def test_temp(self):
-        cells = self.cells[:4].copy()
-        #perturb inital coordinate guesses
-        for cell in cells:
-            cell.coords.xl -= 3
-            cell.coords.xr += 3
-            cell.coords.a0 *= 0.9
-            cell.coords.a1 *= 1.05
-
-        res_list = cells.optimize_mp(minimizer=DifferentialEvolution)
-
-        # Check if the result has been properly substituted in all cell objects
-        for r, cell in zip(res_list, cells):
-            for k, v in r.params.items():
-                self.assertEqual(v, getattr(cell.coords, k))
-
-        obj_values = [23.0, 17.0, 18.0, 22.0]
-        for r, val in zip(res_list, obj_values):
-            self.assertLessEqual(r.objective_value, val + 3)
+    # def test_temp(self):
+    #     cells = self.cells[:4].copy()
+    #     #perturb inital coordinate guesses
+    #     for cell in cells:
+    #         cell.coords.xl -= 3
+    #         cell.coords.xr += 3
+    #         cell.coords.a0 *= 0.9
+    #         cell.coords.a1 *= 1.05
+    #
+    #     res_list = cells.optimize_mp(minimizer=DifferentialEvolution)
+    #
+    #     # Check if the result has been properly substituted in all cell objects
+    #     for r, cell in zip(res_list, cells):
+    #         for k, v in r.params.items():
+    #             self.assertEqual(v, getattr(cell.coords, k))
+    #
+    #     obj_values = [23.0, 17.0, 18.0, 22.0]
+    #     for r, val in zip(res_list, obj_values):
+    #         self.assertLessEqual(r.objective_value, val + 3)
 
 
 if __name__ == '__main__':
