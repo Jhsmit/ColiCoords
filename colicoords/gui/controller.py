32b26b4432b48ebbb3ece2dbabc85753151576a1
Jochem Smit <j.h.smit@rug.nl>
2018-10-23 16:56:41 +0200

    added image select controller

--- colicoords/gui/controller.py
+++ colicoords/gui/controller.py
@@ -1,11 +1,12 @@
-from colicoords.gui.images_select import NavigationWindow, MPLWindow, OverlayImageWindow, PaintOptionsWindow
+from colicoords.gui.images_select import NavigationWindow, MPLWindow, OverlayImageWindow, PaintOptionsWindow, ImageWindow
 from PyQt5.QtCore import pyqtSlot
 from PyQt5 import QtCore
 import queue
 import mahotas as mh
 import numpy as np
 import time
-
+import tifffile
+import os
 
 class NavigationMixin(object):
     index = 0
@@ -348,5 +349,93 @@ class CellObjController(object):
         self.set_frame(self.index + 1)
 
     @pyqtSlot()
+    def _last(self):
+        self.set_frame(self.length - 1)
+
+
+class ImageSelectController(object):
+    index = 0
+    length = 0
+
+    def __init__(self, data, output_path):
+        # data: Data object, image data should be 3d; z, row, column
+        super(ImageSelectController, self).__init__()
+        self.data = data
+        self.length = len(self.data)
+        self.output_path = output_path
+        self.exclude_bools = np.zeros(self.length).astype(bool)
+        self.nw = NavigationWindow()
+        self.iws = []
+        for k, v in self.data.data_dict.items():
+            iw = ImageWindow(v, parent=None, title=k)
+            self.iws.append(iw)
+        self.nw.first_button.clicked.connect(self._first)
+        self.nw.prev_button.clicked.connect(self._prev)
+        self.nw.next_button.clicked.connect(self._next)
+        self.nw.current_frame_text.editingFinished.connect(self._frame_text)
+        self.nw.last_button.clicked.connect(self._last)
+        self.nw.keyPressed.connect(self.key_event_nw)
+        self.nw.exclude_cb.clicked.connect(self._exclude_cb_checked)
+        self.nw.done_button.clicked.connect(self._done)
+        self.nw.closed.connect(self._nw_closed)
+
+    def show(self):
+        for iw in self.iws:
+            iw.show()
+        self.nw.show()
+
+    def _nw_closed(self):
+        for iw in self.iws:
+            iw.close()
+
+    def _done(self):
+        for name, data in self.data.data_dict.items():
+            name = str(name) #Otherwise QString
+            export_data = data[~self.exclude_bools]
+            out_dir = os.path.join(self.output_path, name)
+            if not os.path.exists(out_dir):
+                os.mkdir(out_dir)
+            for i, d in enumerate(export_data):
+                tifffile.imsave(os.path.join(out_dir, name + '_' + str(i).zfill(3)) + '.tif', d)
+        for w in self.iws:
+            w.close()
+        self.nw.close()
+        del self #NUKE em
+    def _exclude_cb_checked(self):
+        self.exclude_bools[self.index] = ~self.exclude_bools[self.index]
+    def exclude(self):
+        self.exclude_bools[self.index] = ~self.exclude_bools[self.index]
+        self.nw.exclude_cb.setChecked(bool(self.exclude_bools[self.index]))
+    def key_event_nw(self, event):
+        if event.key() == QtCore.Qt.Key_Left:
+            self._prev()
+        elif event.key() == QtCore.Qt.Key_A:
+            self._prev()
+        elif event.key() == QtCore.Qt.Key_Right:
+            self._next()
+        elif event.key() == QtCore.Qt.Key_D:
+            self._next()
+        elif event.key() == QtCore.Qt.Key_E:
+            self.exclude()
+    def set_frame(self, i):
+        if i >= self.length:
+            self.index = self.length - 1
+        elif i < 0:
+            self.index = 0
+        else:
+            self.index = i
+        self.nw.current_frame_text.setText(str(self.index))
+        for iw in self.iws:
+            iw.set_frame(self.index)
+        self.nw.exclude_cb.setChecked(bool(self.exclude_bools[self.index]))
+    def _frame_text(self):
+        i = int(self.nw.current_frame_text.text())
+        self.set_frame(i)
+    def _first(self):
+        self.set_frame(0)
+    def _prev(self):
+        self.set_frame(self.index - 1)
+    def _next(self):
+        self.set_frame(self.index + 1)
     def _last(self):
         self.set_frame(self.length - 1)
