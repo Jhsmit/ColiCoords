d4c416b27b6af67b8fdc88a19aa2ead4c91c7bf1
Jochem Smit <j.h.smit@rug.nl>
2018-12-02 18:14:24 +0100

    added test, fixes #9

---
+++ test/test_preprocessing.py
@@ -0,0 +1,36 @@
+import tifffile
+from colicoords.data_models import Data
+from colicoords.fileIO import load_thunderstorm
+from colicoords import data_to_cells
+from test.testcase import ArrayTestCase
+
+
+class TestDataToCells(ArrayTestCase):
+
+    def setUp(self):
+        storm = load_thunderstorm(r'test_data/ds3/storm_table.csv')
+        binary = tifffile.imread(r'test_data/ds3/binary.tif')
+        fluorescence = tifffile.imread(r'test_data/ds3/flu.tif')
+
+        self.storm_ds = Data()
+        self.storm_ds.add_data(binary, 'binary')
+        self.storm_ds.add_data(fluorescence, 'fluorescence')
+        self.storm_ds.add_data(storm, 'storm')
+
+    def test_storm_data_to_cells(self):
+        data = self.storm_ds
+        cells = data_to_cells(data)
+        self.assertEqual(sum([len(c.data.data_dict['storm']) for c in cells]), 40)
+
+
+
+        data_copy = data.copy()
+        storm_ds = data_copy.data_dict['storm']
+        storm_ds['frame'][storm_ds['frame'] == 4] = 5
+
+        cells = data_to_cells(data_copy)
+        num_spots = [0, 10, 0, 6, 1, 14, 1, 0, 0, 0, 0, 0, 0]
+        for c, spots in zip(cells, num_spots):
+            self.assertEqual(len(c.data.data_dict['storm']), spots)
+            self.assertIn('storm', c.data.names)
+            self.assertIn('storm', c.data.dclasses)
