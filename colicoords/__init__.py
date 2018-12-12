75ba1afff13b1b5a3ab3abcdf267d9fa8d952ba8
Jochem Smit <j.h.smit@rug.nl>
2018-12-12 18:53:58 +0100

    added AutoIterCellPlot and align_data_element, updated version temporary hack

--- colicoords/__init__.py
+++ colicoords/__init__.py
@@ -1,9 +1,9 @@
 from colicoords.data_models import Data
 from colicoords.cell import Cell, CellList
 from colicoords.preprocess import data_to_cells, filter_binaries
-from colicoords.postprocess import align_cells, align_images, align_storm
+from colicoords.postprocess import align_data_element, align_images, align_storm
 from colicoords.plot import CellPlot, CellListPlot
-from colicoords.iplot import iter_subplots, IterCellPlot
+from colicoords.iplot import iter_subplots, IterCellPlot, AutoIterCellPlot
 from colicoords.fileIO import load, save, load_thunderstorm
 from colicoords.models import RDistModel, PSF
 from colicoords.fitting import LinearModelFit, CellFit
@@ -15,7 +15,7 @@ import pkg_resources
 try:
     __version__ = pkg_resources.get_distribution('colicoords').version
 except pkg_resources.DistributionNotFound:
-    __version__ = '0.1.1'
+    __version__ = '0.1.3'
 
 try:
     from colicoords.models import Memory
