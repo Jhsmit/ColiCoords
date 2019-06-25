a26e9738b180dfca4145cf8983f779817b268df6
Jochem Smit <j.h.smit@rug.nl>
2019-06-25 12:23:20 +0200

    removed deprecated class

--- colicoords/cnn/preprocess.py
+++ colicoords/cnn/preprocess.py
@@ -254,65 +254,6 @@ class DefaultImgSequence(AugmentedImgSequence):
                                                  augmentation=augmentation, shuffle=shuffle, batch_size=batch_size)
 
 
-class ImgSequence(Sequence):
-    """Deprecated, will be removed"""
-    def __init__(self, x_arr, y_arr, batch_size=10, shuffle=True):
-        assert x_arr.shape == y_arr.shape
-        self.x_arr = x_arr
-        self.y_arr = y_arr
-        self.batch_size=batch_size
-        self.shuffle = shuffle
-        
-        self.operations = [
-            lambda x: x,
-            lambda x: np.transpose(x, axes=(0, 2, 1)),
-            lambda x: x[:, :, ::-1],
-            lambda x: x[:, ::-1, :],
-            lambda x: np.transpose(x, axes=(0, 2, 1))[:, :, ::-1],
-            lambda x: x[:, ::-1, ::-1],
-            lambda x: np.transpose(x, axes=(0, 2, 1))[:, ::-1, :],
-            lambda x: np.transpose(x, axes=(0, 2, 1))[:, ::-1, ::-1]
-        ]
-        
-    def __len__(self):
-        return int(np.floor(len(self.x_arr)*8/self.batch_size))
-    
-    def __getitem__(self, idx):
-        start = idx*self.batch_size
-        stop = (idx+1)*self.batch_size
-        
-        input_len = len(self.x_arr)
-        start_op = start // input_len
-        stop_op = stop // input_len
-
-        start_idx = start % input_len
-        stop_idx = stop % input_len
-
-        if stop_op == 8:
-            x_arr = self.operations[start_op](self.x_arr[start_idx:])
-            y_arr = self.operations[start_op](self.y_arr[start_idx:])
-        
-        elif start_op == stop_op:
-            x_arr = self.operations[start_op](self.x_arr[start_idx:stop_idx])
-            y_arr = self.operations[start_op](self.y_arr[start_idx:stop_idx])
-
-        else:
-            x_arr_1 = self.operations[start_op](self.x_arr[start_idx:])
-            y_arr_1 = self.operations[start_op](self.y_arr[start_idx:]) 
-
-            x_arr_2 = self.operations[stop_op](self.x_arr[0:stop_idx])
-            y_arr_2 = self.operations[stop_op](self.y_arr[0:stop_idx])
-            
-            x_arr = np.concatenate((x_arr_1, x_arr_2))
-            y_arr = np.concatenate((y_arr_1, y_arr_2))
-            
-        if self.shuffle:
-            p = np.random.permutation(self.batch_size)
-            x_arr = x_arr[p]
-            y_arr = y_arr[p]
-
-        return np.expand_dims(x_arr, -1), np.expand_dims(y_arr, -1)
-
 
 def norm_stack(img_stack):
     """
