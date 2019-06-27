21e010c0c498b2a778a3f1e470f48290e10829f9
Jochem Smit <jhsmit@gmail.com>
2019-06-27 10:46:54 +0200

    add try/except to ArrayFitResults (for symfit 0.4.6)

--- colicoords/fitting.py
+++ colicoords/fitting.py
@@ -339,8 +339,11 @@ class LinearModelFit(Fit):
         overall_dict = {**res.params, **linear_dict}
         popt = [overall_dict[par.name] for par in self._old_model.params]
 
-        return ArrayFitResults(self._old_model, popt, None, res.infodict, res.status_message, res.iterations, **res.gof_qualifiers)
-
+        
+        try:
+            return ArrayFitResults(self._old_model, popt, None, res.infodict, res.status_message, res.iterations, **res.gof_qualifiers)
+        except AttributeError:
+            return ArrayFitResults(self._old_model, popt, None, res.minimizer, res.objective, res.status_message, **res.minimizer_output)
 
 def solve_linear_system(y_list, data):
     """Solve system of linear eqns a1*y1 + a2*y2 == data but then also vector edition of that"""
