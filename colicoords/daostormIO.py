cd893983ab13abb36b695fece93a859feb680b87
Hazen Babcock <hbabcock@mac.com>
2019-07-16 08:39:38 -0400

    Replace assertion with a warning.

--- colicoords/daostormIO.py
+++ colicoords/daostormIO.py
@@ -5,6 +5,7 @@ Load 3D-DAOSTORM (https://github.com/ZhuangLab/storm-analysis) format data.
 Hazen 10/13
 """
 import numpy
+import warnings
 
 import colicoords.fileIO as fileIO
 
@@ -22,15 +23,15 @@ def load_daostorm(file_path):
     """
     # At least for now, we only handle tracked files.
     with saH5Py.SAH5Py(file_path) as h5:
-        assert h5.hasTracks(), "No tracks found in HDF5 file."
+        if not h5.hasTracks():
+            warnings.warn("No tracks found in '" + file_path + "' HDF5 file.")
         tracks = h5.getTracks(fields = ['frame_number', 'x', 'y'])
 
-    # Convert to numpy data table.
+    # Change 'frame_number' to 'frame'
     names = list(tracks.keys())
-
-    # Change 'frame' to 'frame_number'
     names = ['frame' if (x == 'frame_number') else x for x in names]
 
+    # Convert to numpy data table.
     dtype = {'names': tuple(names),
              'formats': tuple(fileIO.TYPES[name] for name in names)}
 
