08d47dc4e465599af0145b179e74101d0eb97a62
Jochem Smit <j.h.smit@rug.nl>
2019-09-11 13:46:33 +0200

    chaged y label of celllistplot longitinudial distance

--- colicoords/plot.py
+++ colicoords/plot.py
@@ -1339,7 +1339,7 @@ class CellListPlot(object):
             ax.fill_between(x, mean + width, mean - width, alpha=0.25)
 
         ax.set_xlabel('Distance ({})'.format(xunits))
-        ax.set_ylabel('Signal intensity ({})'.format(yunits))
+        ax.set_ylabel('Intensity ({})'.format(yunits))
         ax.set_title('Longitudinal Distribution')
 
         if norm_y:
