b1acc2111a9a2be3d52d19afffe67b73ad08aac0
Jochem Smit <j.h.smit@rug.nl>
2019-11-11 13:28:38 +0100

    added plot phi dist to itercellplot

--- colicoords/iplot.py
+++ colicoords/iplot.py
@@ -850,6 +850,46 @@ class IterCellPlot(object):
 
         return line
 
+    def plot_phi_dist(self, ax=None, data_name='', r_max=None, r_min=0, storm_weight=False, method='gauss',
+                      dist_kwargs=None, **kwargs):
+        step = kwargs.pop('step', cfg.PHI_DIST_STEP)
+        sigma = kwargs.pop('sigma', cfg.PHI_DIST_SIGMA)
+        dist_kwargs = {} if dist_kwargs is None else dist_kwargs
+
+        if not data_name:
+            try:
+                data_elem = list(self.cell_list[0].data.flu_dict.values())[0]  # yuck
+            except IndexError:
+                try:
+                    data_elem = list(self.cell_list[0].data.storm_dict.values())[0]
+                except IndexError:
+                    raise IndexError('No valid data element found')
+        else:
+            data_elem = self.cell_list[0].data.data_dict[data_name]
+
+        x_vals, phi_l, phi_r = self.cell_list.phi_dist(step, data_name=data_name, r_max=r_max, r_min=r_min,
+                                                      storm_weight=storm_weight, sigma=sigma, method=method, **dist_kwargs)
+
+        ax = plt.gca() if ax is None else ax
+        if data_elem.dclass == 'storm':
+            if storm_weight:
+                ylabel = 'Total STORM intensity (photons)'
+            else:
+                ylabel = 'Number of localizations'
+        else:
+            ylabel = 'Intensity (a.u.)'
+
+        ax.set_xlabel('Distance ({})'.format('degrees'))
+        ax.set_ylabel(ylabel)
+
+        l = kwargs.pop('label', None)
+
+        line_l, = ax.iter_plot([x_vals for x in range(len(self.cell_list))], phi_l)
+        line_r, = ax.iter_plot([x_vals for x in range(len(self.cell_list))], phi_r)
+
+        return line_l, line_r
+
+
     def plot_storm(self, ax=None, data_name='', method='plot', upscale=5, alpha_cutoff=None, storm_weight=False, sigma=0.25, **kwargs):
         #todo make functions with table and shape and other kwargs?
         """
